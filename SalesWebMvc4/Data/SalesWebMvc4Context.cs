dc18f29a5e451554d952e5021c7013250761c96d
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-09-13 17:50:07 -0300

    13092024

---
+++ SalesWebMvc4/Data/SalesWebMvc4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc1.Models;
+
+namespace SalesWebMvc4.Data
+{
+    public class SalesWebMvc4Context : DbContext
+    {
+        public SalesWebMvc4Context (DbContextOptions<SalesWebMvc4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc1.Models.Department> Department { get; set; }
+    }
+}
