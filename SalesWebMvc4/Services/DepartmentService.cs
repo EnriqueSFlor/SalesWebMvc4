4410c1adfa23ab18e715dd28b07f387dce526a86
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-09-17 21:09:26 -0300

    17092024

---
+++ SalesWebMvc4/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc4.Data;
+using SalesWebMvc4.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc4.Services
+{
+    public class DepartmentService
+    {
+
+            private readonly SalesWebMvc4Context _context;
+
+            public DepartmentService(SalesWebMvc4Context context)
+            {
+                _context = context;
+            }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+        }
+    }
